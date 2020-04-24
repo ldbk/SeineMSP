c525216b0200507868be2d4beb2de76f52a1ea66
Jehanne RIVET <jrivet@ifremer.fr>
2020-04-24 10:57:32 +0200

    ACM sur benthos qui pour l'instant ne fonctionne pas

--- analyses/Benthos traits.R
+++ analyses/Benthos traits.R
@@ -1,5 +1,7 @@
 setwd("C:/Users/jrivet/Documents/Stage M2/Data/CGFS")
 library(dplyr)
+library(FactoMineR)
+library(missMDA)
 
 benthos<- read.csv("Traits benthos (BIOTIC).csv", sep=";")
 
@@ -48,3 +50,56 @@ names(benthos)[4]<- "MaturityAge"
   benthos[which(benthos=="No text entered", arr.ind = T)]
   benthos[which(benthos=="No text entered", arr.ind = T)]<- NA
 }
+
+{
+  benthos=="See additional information"
+  which(benthos=="See additional information", arr.ind = T)
+  benthos[which(benthos=="See additional information", arr.ind = T)]
+  benthos[which(benthos=="See additional information", arr.ind = T)]<- NA
+}
+
+{
+  benthos$MaturityAge=="<1 year"
+  which(benthos$MaturityAge=="<1 year", arr.ind = T)
+  benthos$MaturityAge[which(benthos$MaturityAge=="<1 year", arr.ind = T)]
+  benthos$MaturityAge[which(benthos$MaturityAge=="<1 year", arr.ind = T)]<- "< 1 year"
+}
+
+
+
+
+
+
+# ACM
+
+{
+  nbid<- benthos %>%
+    summarise_all(n_distinct) %>%
+    t()
+  nbNA<- function(a){a[a==""]<-NA;sum(is.na(a))}
+  nbNA<- benthos %>%
+    summarise_all(nbNA) %>%
+    t()
+  
+  summary<- data.frame(nbid=nbid,nbNA=nbNA)
+}
+
+essai<- benthos %>%
+  select(SpeciesName, LifeSpan, MaturityAge, Habit, ReprodFreq) 
+
+  # Imputation
+essai<- benthos %>% 
+  mutate_all(as.factor) %>% 
+  data.frame()
+
+row.names(essai)<- essai[,1] 
+essai<- essai[,-1]
+nb<- estim_ncpMCA(essai) # Error : The algorithm fails to converge. Choose a number of components (ncp) less or equal than 2 or a number of iterations (maxiter) less or equal than 999
+
+rez<- imputeMCA(essai, ncp=4)
+rez<- MCA(essai, tab.disj=rez$tab.disj.comp)
+HCPC(rez)
+
+
+
+
