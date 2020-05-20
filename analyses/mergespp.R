44bfa9cf660e4cc227e254d3c79ec228fed1fc60
dbklor <wuchwusul@gmail.com>
2020-05-20 17:43:22 +0200

    merge trait updated

--- analyses/mergespp.R
+++ analyses/mergespp.R
@@ -13,18 +13,15 @@ spp0<-spp0%>%mutate(species=ifelse(species=="trutta","trutta trutta",species))
 #if Psetta maxima -> Scophthalmus maximus to match Beuhkof name
 spp0<-spp0%>%mutate(genus=ifelse(genus=="Psetta","Scophthalmus",genus))
 spp0<-spp0%>%mutate(species=ifelse(genus=="Psetta","maximus",species))
-
-#write.csv(spp0,file="spp0.csv")
-
 #match Beukhof with spp list
 trait<-readxl::read_excel("../data/fishtrait/TraitCollectionFishNAtlanticNEPacificContShelf.xlsx")%>%
 	distinct()
 trait22<-trait%>%filter(LME==22)
-trait22gen<-trait%>%filter(LME==22&is.na(species))
-trait22fam<-trait%>%filter(LME==22&is.na(genus))
+trait22gen<-trait%>%filter(LME==22&is.na(species))%>%select(-species)
+trait22fam<-trait%>%filter(LME==22&is.na(genus))%>%mutate(genus=family)%>%select(-species)
 trait24<-trait%>%filter(LME==24)
-trait24gen<-trait%>%filter(LME==24&is.na(species))
-trait24fam<-trait%>%filter(LME==24&is.na(genus))
+trait24gen<-trait%>%filter(LME==24&is.na(species))%>%select(-species)
+trait24fam<-trait%>%filter(LME==24&is.na(genus))%>%mutate(genus=family)%>%select(-species)
 
 #successive join
 #spp ok in LME 22
@@ -39,37 +36,40 @@ spptmp<-anti_join(spptmp,gen22)
 gen24<-left_join(spptmp,trait24gen,by=c("genus"))%>%filter(is.finite(LME))
 spptmp<-anti_join(spptmp,gen22)
 #family in 22 & 24
-fam22<-left_join(spptmp,trait22fam,by=c("genus"="family"))%>%filter(is.finite(LME))
+fam22<-left_join(spptmp,trait22fam,by=c("genus"="genus"),keep=T)%>%filter(is.finite(LME))
 spptmp<-anti_join(spptmp,fam22)
-fam24<-left_join(spptmp,trait24fam,by=c("genus"="family"))%>%filter(is.finite(LME))
+fam24<-left_join(spptmp,trait24fam,by=c("genus"="genus"),keep=T)%>%filter(is.finite(LME))
 spptmp<-anti_join(spptmp,fam24)
+#identify cephalo using a file generated manually
+listceph<-c("Alloteuthis","Eledone cirrhosa","Loligo","Loligo forbesi","Loligo vulgaris","Sepia officinalis","Sepiola")
+listceph<-data.frame(spp=listceph,ceph=1)
+spptmp<-anti_join(spptmp,listceph)
+
 #match spptmp with BENTIC: 1 pass
 #some manual correction to match BENTIC available species (because genus
 #matching is not available
 spptmp<-spptmp%>%mutate(species=ifelse(spp=="Maja brachydactyla","squinado",species))
 spptmp<-spptmp%>%mutate(species=ifelse(spp=="Pagurus prideaux","prideauxi",species))
 spptmp<-spptmp%>%mutate(genus=ifelse(spp=="Mimachlamys varia","Chlamys",genus))
-spptmp<-spptmp%>%mutate(species=ifelse(spp=="Macropodia rostrata","spp",species))
-spptmp<-spptmp%>%mutate(species=ifelse(spp=="Galathea","spp",species))
+spptmp<-spptmp%>%mutate(species=ifelse(spp=="Macropodia rostrata","spp.",species))
+spptmp<-spptmp%>%mutate(species=ifelse(spp=="Galathea","spp.",species))
 spptmp<-spptmp%>%mutate(species=ifelse(spp=="Pagurus cuanensis","bernhardus",species))
 spptmp<-spptmp%>%mutate(species=ifelse(spp=="Hippocampus","hippocampus",species))
-spptmp<-spptmp%>%mutate(genus=ifelse(is.na(genus),"",genus))%>%
+spptmp<-spptmp%>%mutate(species=ifelse(spp=="Palaemon serratus","spp.",species))
+#for species with NA species put spp.
+spptmp<-spptmp%>%mutate(species=ifelse(is.na(species),"spp.",species))%>%
 	mutate(spp2=paste(genus,species))
-				      
 write.csv(spptmp%>%transmute(spp2,spp),file="spptmp.csv",row.names=F)
 
-benthos1<-read.csv("BIOTIC.csv",sep=",",header=T)
-spptmp<- anti_join(spptmp,benthos1,by=c("spp2"="BioticID"))
-#View(spptmp)
+#use spptmp.csv (after conversion in excel file) to get BIOTIC info from the website then read it
+benthos<-read.table("BIOTIC.csv",sep=";",header=T,strip.white=T,fill=T)
+benthos<-left_join(benthos,spptmp,by=c("SpeciesName"="spp2"))
 
-#match spptmp genus with BENTIC: 2 pass
-#write.csv(spptmp%>%transmute(genus=paste(genus,genus),spp),file="spptmp.csv")
-
-#benthos<- read.csv2("../data/Traits benthos (BIOTIC).csv")
-#sppben<-benthos%>%transmute(spp=SpeciesName)%>%distinct()
-#spp
+#species with no match:
+spptmp<- anti_join(spptmp,benthos,by=c("spp"="spp"))
 
 
+#a summary
 sumrez<-data.frame(nbinit=nrow(spp0),
 		   nbspp22=nrow(spp22),
 		   nbspp24=nrow(spp24),
@@ -77,7 +77,16 @@ sumrez<-data.frame(nbinit=nrow(spp0),
 		   nbgen24=nrow(gen24),
 		   nbfam22=nrow(fam22),
 		   nbfam24=nrow(fam24),
-		   nbbentho1=nrow(benthos1)
+		   nbceph=nrow(listceph),
+		   nbbenthos=nrow(benthos)
 		   )
 sumrez$remain<-sumrez$nbinit-sum(sumrez[2:length(sumrez)])
 print(sumrez)
+
+#final file
+traitfish<-rbind(spp22,spp24,gen22,fam22)
+traitbenthos<-benthos
+traitceph<-listceph
+
+
+
