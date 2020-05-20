f2c569b0f6f4918dddc7d750e37ae1f331c8ffd7
Jehanne RIVET <jrivet@ifremer.fr>
2020-05-20 09:44:40 +0200

    nouveau

--- analyses/essai jointures.R
+++ analyses/essai jointures.R
@@ -13,26 +13,26 @@ Espdatras<- read.csv("TabespecesDATRAS.csv")
 names(Espdatras)[2]<- "Species"
 Espdatras$Species<- as.character(Espdatras$Species)
 
-# Pangea traits
+# Especes LME=22
 Beukhof<- readxl::read_excel("C:/Users/jrivet/Documents/Stage M2/SeineMSP/data/fishtrait/TraitCollectionFishNAtlanticNEPacificContShelf.xlsx")
-Beukhof<- Beukhof %>% dplyr::select(family, genus, species, LME,habitat,feeding.mode,tl,age.maturity,growth.coefficient,length.max,age.max) %>%
+Beukhof1<- Beukhof %>% dplyr::select(family, genus, species, LME,habitat,feeding.mode,tl,age.maturity,growth.coefficient,length.max,age.max) %>%
   filter(LME==22)
 
 
-Beukhofspe0<- Beukhof %>% select(genus, species) %>%  na.omit %>%
+Beukhofspe0<- Beukhof1 %>% select(genus, species) %>%  na.omit %>%
   mutate(Taxon=paste(genus, species))
-Beukhofspe1<- Beukhof %>% left_join(Beukhofspe0, by=c("genus", "species"))
+Beukhofspe1<- Beukhof1 %>% left_join(Beukhofspe0, by=c("genus", "species"))
 Beukhofspe1<- unique(Beukhofspe1)
 Beukhofspe<- Beukhofspe1 %>% filter(!is.na(Taxon))
 Beukhofspe<- Beukhofspe[,-c(1, 2, 3, 4)]
 Beukhofspe<- Beukhofspe[, c(8,1, 2, 3, 4, 5, 6, 7 )]
 
-Beukhofgen<- Beukhof %>% select(genus, species, LME,habitat,feeding.mode,tl,age.maturity,growth.coefficient,length.max,age.max)
+Beukhofgen<- Beukhof1 %>% select(genus, species, LME,habitat,feeding.mode,tl,age.maturity,growth.coefficient,length.max,age.max)
 Beukhofgen<- Beukhofgen %>% filter(is.na(Beukhofgen$species),!is.na(Beukhofgen$genus))
 names(Beukhofgen)[1]<- "Taxon"
 Beukhofgen<- Beukhofgen[,-c(2, 3)]
 
-Beukhoffam<- Beukhof %>% select(family, genus, species, LME,habitat,feeding.mode,tl,age.maturity,growth.coefficient,length.max,age.max)
+Beukhoffam<- Beukhof1 %>% select(family, genus, species, LME,habitat,feeding.mode,tl,age.maturity,growth.coefficient,length.max,age.max)
 Beukhoffam<- Beukhoffam %>% filter((is.na(Beukhoffam$species)), (is.na(Beukhoffam$genus)), (!is.na(Beukhoffam$family)))
 names(Beukhoffam)[1]<- "Taxon"
 Beukhoffam<- Beukhoffam[,-c(2, 3, 4)]
@@ -50,15 +50,49 @@ traits<- TotBenkhof %>% filter(Taxon %in% Espdatras$Species) %>%
 missing<- Espdatras %>% filter(!Species %in% TotBenkhof$Taxon) %>%
   mutate(Reconnue= "No")
 
-missing<- Espdatras %>% filter(Species %in% TotBenkhof$Taxon) %>%
-  mutate(Reconnue= "No")
+
+
+# Especes LME=24
+
+Beukhof2<- Beukhof %>% dplyr::select(family, genus, species, LME,habitat,feeding.mode,tl,age.maturity,growth.coefficient,length.max,age.max) %>%
+  filter(LME==24)
+
+
+Beukhofspe0Z<- Beukhof2 %>% select(genus, species) %>%  na.omit %>%
+  mutate(Taxon=paste(genus, species))
+Beukhofspe1Z<- Beukhof2 %>% left_join(Beukhofspe0Z, by=c("genus", "species"))
+Beukhofspe1Z<- unique(Beukhofspe1Z)
+BeukhofspeZ<- Beukhofspe1Z %>% filter(!is.na(Taxon))
+BeukhofspeZ<- BeukhofspeZ[,-c(1, 2, 3, 4)]
+BeukhofspeZ<- BeukhofspeZ[, c(8,1, 2, 3, 4, 5, 6, 7 )]
+
+BeukhofgenZ<- Beukhof2 %>% select(genus, species, LME,habitat,feeding.mode,tl,age.maturity,growth.coefficient,length.max,age.max)
+BeukhofgenZ<- BeukhofgenZ %>% filter(is.na(BeukhofgenZ$species),!is.na(BeukhofgenZ$genus))
+names(BeukhofgenZ)[1]<- "Taxon"
+BeukhofgenZ<- BeukhofgenZ[,-c(2, 3)]
+
+BeukhoffamZ<- Beukhof2 %>% select(family, genus, species, LME,habitat,feeding.mode,tl,age.maturity,growth.coefficient,length.max,age.max)
+BeukhoffamZ<- BeukhoffamZ %>% filter((is.na(BeukhoffamZ$species)), (is.na(BeukhoffamZ$genus)), (!is.na(BeukhoffamZ$family)))
+names(BeukhoffamZ)[1]<- "Taxon"
+BeukhoffamZ<- BeukhoffamZ[,-c(2, 3, 4)]
+
+
+TotBenkhofZ<- BeukhofspeZ %>% bind_rows(BeukhofspeZ, BeukhofgenZ, BeukhoffamZ)
+TotBenkhofZ<- unique(TotBenkhofZ)
+TotBenkhofZ$Taxon<- as.character(TotBenkhofZ$Taxon)
+
+
+traitsZ<- TotBenkhofZ %>% filter(Taxon %in% Espdatras$Species) %>%
+  mutate(Reconnue= "Yes")
+
+traitsZ<- traitsZ %>% filter(!Taxon %in% traits$Taxon)
 
 
 
+# Jointure des deux
 
-
-missing<- Espdatras$Species[!which(Espdatras$Species %in% TotBenkhof$Taxon)]
-missing<- unique(missing)
+Joint<- traits %>% bind_rows(traits, traitsZ)
+Joint<- unique(Joint)
 
 
 
