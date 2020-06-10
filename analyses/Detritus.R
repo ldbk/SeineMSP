907a4d8e097469f0a7159f4de845353cba7d19ae
Jehanne RIVET <jrivet@IFREMER.FR>
2020-06-10 13:55:01 +0200

    turbidity & detritus script & data

---
+++ analyses/Detritus.R
@@ -0,0 +1,148 @@
+library(raster)
+library(rasterVis)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(viridis)
+library(dplyr)
+library(tidyr)
+
+Detrit<- stack("data/satellite/Detritus/cdm443")
+
+
+# Conversion raster - tableau
+fortify.Raster <- function(Detrit, maxPixel = 1000000) {
+  
+  if (ncell(Detrit) > maxPixel) {
+    x <- sampleRegular(Detrit, maxPixel, asRaster=TRUE)
+  }
+  xy <- xyFromCell(Detrit, seq_len(ncell(Detrit)))
+  out <- Detrit %>%
+    getValues() %>%
+    data.frame(values = .) %>%
+    cbind(xy)
+  return(out)
+}
+
+
+# Traitement tableau
+TabDet<- fortify(Detrit)
+pixelok<- which(!is.na(apply(TabDet,1,mean)))
+TabDet<- pivot_longer(TabDet, cols=1:244, names_to = "Date", values_to = "Detritus", values_drop_na = TRUE)
+
+TabDet$Date<-sub("values.index_","",TabDet$Date)
+TabDet$Year<- as.numeric(substr(as.character(TabDet$Date),1,4))
+TabDet$Month<- as.numeric(substr(as.character(TabDet$Date), 6,7))
+
+
+# Infos
+mean(TabDet$Detritus)
+min(TabDet$Detritus)
+max(TabDet$Detritus)
+sd(TabDet$Detritus)
+var(TabDet$Detritus)
+
+
+# Mean detritus per year
+TabDet2<- TabDet %>% group_by(x,y,Year) %>% summarize(moyDet= mean(Detritus))
+ggplot(TabDet2)+
+  geom_tile(aes(x=x, y=y, fill=moyDet))+
+  ggtitle("Detritus moyenne 1997-2017")+
+  facet_wrap(. ~Year)+
+  xlab("Longitude")+
+  ylab("Latitude")+
+  theme_minimal()+
+  scale_fill_gradientn(colours = terrain.colors(6))  
+
+ggplot(TabDet2, aes(x=Year, y=moyDet, group=Year))+
+  geom_boxplot()
+
+
+# Mean detritus 1997-2017
+TabDet3<- TabDet2 %>% group_by(x,y) %>% summarize(moyper= mean(moyDet))
+ggplot(TabDet3)+
+  geom_tile(aes(x=x, y=y, fill= moyper))+
+  ggtitle("Detritus moyenne 1997-2017")+
+  xlab("Longitude")+
+  ylab("Latitude")+
+  theme_minimal()+
+  scale_fill_gradientn(colours = terrain.colors(6))  
+
+
+# Serie tempo mean detritus
+TabDet4<- TabDet %>% group_by(Month) %>% summarize(moybaie= mean(Detritus))
+ggplot(TabDet4)+
+  geom_line(aes(x=Month, y= moybaie))+
+  ggtitle("Detritus mensuels 1997-2017")+
+  xlab("Month")+
+  ylab("Detritus")+
+  theme_minimal()+
+  scale_fill_gradientn(colours = terrain.colors(6))  
+
+
+
+
+
+
+
+# Partitionnement
+
+TabDetnew<- pivot_wider(TabDet2, names_from = Year, values_from = moyDet)
+TabDetnew<- na.omit(TabDetnew)
+metaTabnew<- TabDetnew %>% dplyr::select(x, y)
+TabDetnew<- TabDetnew %>% ungroup() %>% dplyr::select(-x, -y)
+
+distance<- dist(TabDetnew)
+distance[1:5]
+
+tree<- hclust(distance, )
+plot(tree)
+
+rect.hclust(tree, 5)
+zones<- cutree(tree, 5)
+print(zones)
+
+zone<- Detrit[[1]]
+values(zone)<- NA
+zone[pixelok]<- zones
+plot(zone, xlab="Longitude", ylab="Latitude")
+
+
+# Raster
+r0<- raster(nrow=80, ncol=100, xmn=-1.500034, xmx=0.7083337, ymn=49.16667, ymx=49.70833)
+projection(r0)<- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+
+r1<- raster::rasterize(metaTabnew, r0, fields=zones, fun=mean)
+plot(r1)
+
+toto <- cbind(metaTabnew, Clust=factor(zones))
+head(toto)
+
+
+
+TabDetnew<- bind_cols(TabDetnew, metaTabnew)
+TabDetnew<- pivot_longer(TabDetnew, cols = 1:21, names_to = "Year", values_to = "moyDet")
+
+essai<- left_join(TabDetnew, toto, by=c("x", "y"))
+
+for (k in unique(essai[,"Clust"])){
+  essai2<- essai %>%  group_by(Clust) %>% summarise(mean= mean(moyDet)) }
+
+toto2<- left_join(toto, essai2, by="Clust")
+
+ggplot(toto2)+
+  geom_tile(aes(x=x,y=y,fill=mean))+
+  xlab("Longitude")+
+  ylab("Latitude")+
+  labs(fill="")+
+  theme_minimal()+
+  coord_fixed()
+
+
+
+
+
+
+
+
+
